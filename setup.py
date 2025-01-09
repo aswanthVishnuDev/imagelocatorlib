0a10ec0816ddfad73662f8e18c9a53b5960a0d5e
Aswanth Ravikumar Jaya <336155@NTTDATA.COM>
2025-01-09 15:59:27 +0530

    initial commit2

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="my_imagelocatorlib",
     version="0.1.0",
     author="Aswanth",
     author_email="aswanth.ravikumarjaya@nttdata.com",
     description="A library for locating image in a windows screen",
-    long_description=open("README.md"),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/aswanthVishnuDev/imagelocatorlib",
     packages=find_packages(),
