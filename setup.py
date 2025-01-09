6095ad11f187ccfb2d2a1cbeaa58fa2ccee2318c
Aswanth Ravikumar Jaya <336155@NTTDATA.COM>
2025-01-09 17:03:50 +0530

    initial commit4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="imagelocatorlib",
+    name="locator_library",
     version="0.1.0",
     author="Aswanth",
     author_email="aswanth.ravikumarjaya@nttdata.com",
